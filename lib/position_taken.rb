4d917a868779c106e5d782596e8b7933b5f3e35c
Michael Nnaji <mnnaji3@uwo.ca>
2019-02-17 21:22:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 def position_taken?(board, index)
   if board[index] == " " || board[index] =="" ||  board[index] ==nil
     false
   elsif board[index] == "X" || board[index]= "O"
     true
   end
+=======
+# code your #position_taken? method here!
+
+def position_taken?(board,index)
+  if board[index] == "X" || "O"
+    puts "true"
+    end
+>>>>>>> bbe704f3c0d380b74d703fe72fca597b7d44c44d
 end
